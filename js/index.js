247112a82352254e3dc9d87bee5c86301a20b1ab
drawnocat <isabeldrano@gmail.com>
2023-05-14 15:16:02 -0700

    jquery done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-
-console.log("Your index.js file is loaded correctly!:");
-    
+document.getElementById("hireMe").addEventListener("click", function () {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+  
+    setTimeout(function () {
+      modal.style.display = "none";
+    }, 3000); // 3 seconds
+  });
